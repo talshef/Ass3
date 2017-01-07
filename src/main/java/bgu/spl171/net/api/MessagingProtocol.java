2b24a63a9c6ae6d4bb35ba646f5785e47551aa7e
IdanLeitman <IdanLeitman@Idan>
2017-01-07 19:58:17 +0200

    fix vgc

--- src/main/java/bgu/spl171/net/api/MessagingProtocol.java
+++ src/main/java/bgu/spl171/net/api/MessagingProtocol.java
@@ -5,7 +5,7 @@ public interface MessagingProtocol<T> {
     /**
      * process the given message 
      * @param msg the received message
-     * @return the response to send or null if no response is expected by the client
+     * @retdfdurn the response to send or null if no response is expected by the client
      */
     T process(T msg);
  
