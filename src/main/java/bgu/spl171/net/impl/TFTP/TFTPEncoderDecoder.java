7e75f5454f634afd604ea99824ba4610f9f83a4f
IdanLeitman <IdanLeitman@Idan>
2017-01-15 18:48:27 +0200

    new EncoderDecoder class

---
+++ src/main/java/bgu/spl171/net/impl/TFTP/TFTPEncoderDecoder.java
@@ -0,0 +1,19 @@
+package bgu.spl171.net.impl.TFTP;
+
+import bgu.spl171.net.api.MessageEncoderDecoder;
+
+public class TFTPEncoderDecoder implements MessageEncoderDecoder<Packet> {
+
+	@Override
+	public Packet decodeNextByte(byte nextByte) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] encode(Packet message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
