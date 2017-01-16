31e812eaadc37d90bbaad84847e5e24ced317701
talshef <tsheffer@gmail.com>
2017-01-16 16:19:10 +0200

    asss

---
+++ src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
@@ -0,0 +1,26 @@
+package bgu.spl171.net.impl.TFTPtpc;
+
+import bgu.spl171.net.impl.TFTP.TFTPEncoderDecoder;
+import bgu.spl171.net.impl.TFTP.TFTPProtocol;
+import bgu.spl171.net.impl.newsfeed.NewsFeed;
+import bgu.spl171.net.impl.rci.ObjectEncoderDecoder;
+import bgu.spl171.net.impl.rci.RemoteCommandInvocationProtocol;
+import bgu.spl171.net.srv.Server;
+import bgu.spl171.net.impl.TFTP.*;
+
+public class TPCMain {
+	 public static void main(String[] args) {
+	       System.out.println("dd");
+
+	// you can use any server... 
+//	        Server.fixedThreadPool(
+//	                8,
+//	                7777, //port
+//	                () -> new RemoteCommandInvocationProtocol<>(feed), //protocol factory
+//	                ObjectEncoderDecoder::new //message encoder decoder factory
+//	        ).serve();
+
+	        Server.threadPerClient(Integer.parseInt(args[0]), ()-> new TFTPProtocol() , ()->new TFTPEncoderDecoder()).serve();
+
+	    }
+}
